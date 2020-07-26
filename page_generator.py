73a662675dff4f714cd88c73ba15d3dcba6f8b39
adityan18 <aditya.sushma2001@gmail.com>
2020-07-26 17:39:43 +0530

    Evolution need to be added

--- page_generator.py
+++ page_generator.py
@@ -1,9 +1,11 @@
 import re
+import json
 
 filename = "template.html"
 # id_text, name, gen, type_1, type_2, total, hp, attack, defence, sp_atk, sp_def, speed
 
 
+type_color = json.load(open('types_color.json','r'))
 def html_creator(id_text, name, gen, type_1, type_2, total, hp, attack, defence, sp_atk, sp_def, speed):
 
     with open(filename, 'r+') as f:
@@ -13,8 +15,16 @@ def html_creator(id_text, name, gen, type_1, type_2, total, hp, attack, defence,
         text = re.sub('NAME', name, text)
 
         text = re.sub('GEN', 'Gen'+ str(gen), text)
-        text = re.sub('TYPE_L', type_1.lower(), text)
-        text = re.sub('TYPE', type_1, text)
+        text = re.sub('TYPE1_L', type_1.lower(), text)
+        text = re.sub('TYPE1', type_1, text)
+
+        if(type_2 == 'null'):
+            text = re.sub('<li class="TYPE2_L">TYPE2</li>', '', text)
+        else:
+            text = re.sub('TYPE2_L', type_2.lower(), text)
+            text = re.sub('TYPE2', type_2, text)
+
+        text = re.sub('COLOR', type_color[type_1], text)
 
         text = re.sub('TOTAL', str(total), text)
         text = re.sub('hp', str(hp), text)
@@ -34,4 +44,4 @@ def html_creator(id_text, name, gen, type_1, type_2, total, hp, attack, defence,
         new_file.truncate()
         print('HTML Created')
 
-
+html_creator(1, 'Charmander', 1, 'fire', 'null', 10, 10, 10, 10, 10, 10, 10)
