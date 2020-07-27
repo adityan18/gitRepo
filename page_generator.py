171b3528d333f5e906731c4ca6682f20d9ad9acd
adityan18 <aditya.sushma2001@gmail.com>
2020-07-27 12:57:59 +0530

    Images added

--- page_generator.py
+++ page_generator.py
@@ -1,13 +1,19 @@
 import re
 import json
+import re
+import pandas as pd
+from time import time
+start = time()
 
-filename = "template.html"
-# id_text, name, gen, type_1, type_2, total, hp, attack, defence, sp_atk, sp_def, speed
-
-
+pokemon_data = pd.read_csv('pokemon.csv', index_col='ID')
 type_color = json.load(open('types_color.json','r'))
-def html_creator(id_text, name, gen, type_1, type_2, total, hp, attack, defence, sp_atk, sp_def, speed):
 
+
+def html_creator(name, gen, type_text, total, hp, attack, defence, sp_atk, sp_def, speed):
+
+    type_1 = type_text[0]
+    type_2 = type_text[1]
+    filename = "template.html"
     with open(filename, 'r+') as f:
 
         text = f.read()
@@ -44,4 +50,26 @@ def html_creator(id_text, name, gen, type_1, type_2, total, hp, attack, defence,
         new_file.truncate()
         print('HTML Created')
 
-html_creator(1, 'Charmander', 1, 'fire', 'null', 10, 10, 10, 10, 10, 10, 10)
+
+for i in range(len(pokemon_data)):
+    row = pokemon_data.loc[i+1]
+    name = row[0]
+    gen = row[1]
+    type_text = re.findall("'(\S+)'",row[2])
+    total = row[3]
+    hp = row[4]
+    attack = row[5]
+    defence = row[6]
+    sp_atk = row[7]
+    sp_def = row[8]
+    speed = row[9]
+    prev_name = row[10]
+    next_name = row[11]
+    
+    html_creator(name, gen, type_text, total, hp, attack, defence, sp_atk, sp_def, speed)
+    break
+
+
+
+stop = time()
+print('Time:', stop-start)
